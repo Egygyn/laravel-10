b32faf10ad5444cea0c392b2ce8d85395b90bdbf
Egy Gynanjar <egygynanjar96@gmail.com>
2024-05-14 13:53:04 +0700

    tambahin migrate

--- database/migrations/2024_05_08_073538_create_rumahs_table.php
+++ database/migrations/2024_05_08_073538_create_rumahs_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('rumahs', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('type_rumah', 50)->nullable();
+            $table->float('harga_rumah')->nullable();
+            $table->string('lokasi_rumah')->nullable();
             $table->timestamps();
         });
     }
